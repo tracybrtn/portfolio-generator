959c8ecd4238823703fe590468cd276ba78d8502
tracybrtn <tracynburton@gmail.com>
2022-09-09 13:25:35 -0500

    finished HTML

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
 return inquirer.prompt([
@@ -132,12 +134,17 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
-    });
+        const pageHTML = generatePage(portfolioData);
 
-/* const FileSystem = require('fs');
-const generatePage = require('./src/page-template.js');
+    fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
 
+       console.log('Page created! Check out index.html in this directory to see it!');
+     });
+  });
+
+ 
+/*
 const pageHTML = generatePage(name,github);
 
 
