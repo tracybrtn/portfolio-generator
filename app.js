010beb712be32c83aafe1cc787c7634fff61a888
tracybrtn <tracynburton@gmail.com>
2022-09-09 10:15:48 -0500

    working inquirer

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
-const FileSystem = require('fs');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+/* const FileSystem = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+const pageHTML = generatePage(name,github);
 
 
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-  
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-  };
-
-FileSystem.writeFile('index.html', generatePage(name, github), err => {
+FileSystem.writeFile('index.html', pageHTML, err => {
     if (err) throw err;
+
     console.log('Portfolio complete! Check out index.html to see the output!');
-});
+}); */
